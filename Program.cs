bf12a75d4fc46cc65b491be9a1b05ea5c9fa0f20
Matt Blodgett <thailandmatt@gmail.com>
2022-11-30 22:04:50 -0700

    Day 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,44 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
+            Day1Part1();
         }
+
+        #region Day1
+
+        static void Day1Part1()
+        {
+            var fileLines = System.IO.File.ReadAllLines("Day1.txt");
+
+            int max = 0;
+            int cur = 0;
+            List<int> totals = new List<int>();
+
+            foreach (var line in fileLines)
+            {
+                if (line == "")
+                {
+                    totals.Add(cur);
+                    cur = 0;
+                }
+                else
+                {
+                    cur += int.Parse(line);
+                }
+            }
+
+            totals.Add(cur);
+
+            totals.Sort();
+            totals.Reverse();
+
+            Console.WriteLine(totals[0]);
+            Console.WriteLine(totals[1]);
+            Console.WriteLine(totals[2]);
+            Console.WriteLine(totals[0] + totals[1] + totals[2]);
+            Console.ReadLine();
+        }
+
+        #endregion
     }
 }
