2c51381f536e07a5cf4b828a47e01fd97febcd9b
Matt Blodgett <thailandmatt@gmail.com>
2022-12-01 22:08:46 -0700

    Day2

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace AdventOfCode2022
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Day1Part1();
+        {            
+            Day2Part2();
         }
 
         #region Day1
@@ -45,6 +45,81 @@ namespace AdventOfCode2022
             Console.WriteLine(totals[1]);
             Console.WriteLine(totals[2]);
             Console.WriteLine(totals[0] + totals[1] + totals[2]);
+            Console.ReadLine();
+        }
+
+        #endregion
+
+        #region Day2
+
+        static void Day2Part1()
+        {
+            var fileLines = System.IO.File.ReadAllLines("Day2.txt");
+            int score = 0;
+            string theirs = "ABC";
+            string mine = "XYZ";
+            
+            foreach (var line in fileLines)
+            {
+                var parts = line.Split(' ');
+                int theirChoice = theirs.IndexOf(parts[0][0]);
+                int myChoice = mine.IndexOf(parts[1][0]);
+
+                score += myChoice + 1;
+
+                if (theirChoice == myChoice)
+                    score += 3;
+                if (theirChoice == myChoice - 1 || theirChoice == 2 && myChoice == 0)
+                    score += 6;
+            }
+
+            Console.WriteLine(score);
+            Console.ReadLine();
+        }
+
+        static void Day2Part2()
+        {
+            var fileLines = System.IO.File.ReadAllLines("Day2.txt");
+            int score = 0;
+            string theirs = "ABC";            
+
+            //X = lose
+            //Y = draw
+            //Z = win
+
+            foreach (var line in fileLines)
+            {
+                var parts = line.Split(' ');
+                int theirChoice = theirs.IndexOf(parts[0][0]);
+                int myChoice = -1;
+
+                if (parts[1][0] == 'X')
+                {
+                    //lose
+                    myChoice = theirChoice - 1;
+                    if (myChoice == -1) myChoice = 2;
+                }
+                else if (parts[1][0] == 'Y')
+                {
+                    //draw
+                    myChoice = theirChoice;
+                }
+                else if (parts[1][0] == 'Z')
+                {
+                    //win
+                    myChoice = theirChoice + 1;
+                    if (myChoice == 3) myChoice = 0;
+                }
+
+                score += myChoice + 1;
+
+                if (theirChoice == myChoice)
+                    score += 3;
+                if (theirChoice == myChoice - 1 || theirChoice == 2 && myChoice == 0)
+                    score += 6;
+            }
+
+            Console.WriteLine(score);
             Console.ReadLine();
         }
 
