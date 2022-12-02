6814adb446f4ad9283f8ce5b0f62f5ae09309e87
Matt Blodgett <thailandmatt@gmail.com>
2022-12-02 09:22:57 -0700

    Refactor for the snobs

---
+++ Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day1
+    {
+        public static void Day1Part1()
+        {
+            var fileLines = System.IO.File.ReadAllLines("Day1.txt");
+
+            int cur = 0;
+            List<int> totals = new List<int>();
+
+            foreach (var line in fileLines)
+            {
+                if (line == "")
+                {
+                    totals.Add(cur);
+                    cur = 0;
+                }
+                else
+                {
+                    cur += int.Parse(line);
+                }
+            }
+
+            totals.Add(cur);
+
+            totals.Sort();
+            totals.Reverse();
+
+            Console.WriteLine(totals[0]);
+            Console.WriteLine(totals[1]);
+            Console.WriteLine(totals[2]);
+            Console.WriteLine(totals[0] + totals[1] + totals[2]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day2
+    {
+        public static void Day2Part1()
+        {
+            var fileLines = System.IO.File.ReadAllLines("Day2.txt");
+            int score = 0;
+            string theirs = "ABC";
+            string mine = "XYZ";
+            
+            foreach (var line in fileLines)
+            {
+                var parts = line.Split(' ');
+                int theirChoice = theirs.IndexOf(parts[0][0]);
+                int myChoice = mine.IndexOf(parts[1][0]);
+
+                score += myChoice + 1;
+
+                if (theirChoice == myChoice)
+                    score += 3;
+                if (theirChoice == myChoice - 1 || theirChoice == 2 && myChoice == 0)
+                    score += 6;
+            }
+
+            Console.WriteLine(score);
+            Console.ReadLine();
+        }
+
+        public static void Day2Part2()
+        {
+            var fileLines = System.IO.File.ReadAllLines("Day2.txt");
+            int score = 0;
+            string theirs = "ABC";            
+
+            //X = lose
+            //Y = draw
+            //Z = win
+
+            foreach (var line in fileLines)
+            {
+                var parts = line.Split(' ');
+                int theirChoice = theirs.IndexOf(parts[0][0]);
+                int myChoice = -1;
+
+                if (parts[1][0] == 'X')
+                {
+                    //lose
+                    myChoice = theirChoice - 1;
+                    if (myChoice == -1) myChoice = 2;
+                }
+                else if (parts[1][0] == 'Y')
+                {
+                    //draw
+                    myChoice = theirChoice;
+                }
+                else if (parts[1][0] == 'Z')
+                {
+                    //win
+                    myChoice = theirChoice + 1;
+                    if (myChoice == 3) myChoice = 0;
+                }
+
+                score += myChoice + 1;
+
+                if (theirChoice == myChoice)
+                    score += 3;
+                if (theirChoice == myChoice - 1 || theirChoice == 2 && myChoice == 0)
+                    score += 6;
+            }
+
+            Console.WriteLine(score);
+            Console.ReadLine();
+        }
+    }
+}
